161798d2889515d31667c7ec6c33f44da2ac6fb1
GaneshJondhalekar <ganeshjondhalekar3@gmail.com>
2024-05-24 10:15:18 +0530

    Tree updated

--- Tree/BST.py
+++ Tree/BST.py
@@ -34,8 +34,41 @@ class BstTree:
             self.inorder(root.left)
             print(root.data)
             self.inorder(root.right)
-            
             return
+        
+    def preorder(self,root):
+        if root:
+            print(root.data)
+            self.preorder(root.left)
+            self.preorder(root.right)
+            return
+    def postorder(self,root):
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            print(root.data)
+            return
+    def search(self,target,root):
+        if root:
+            if target==root.data:
+                print("Target found")
+            elif target<root.data:
+                if root.left:
+                    self.search(target,root.left)
+                else:
+                    print("Target not found")
+            else:
+                if root.right:
+                    self.search(target,root.right)
+                else:
+                    print("Target not found")
+            return
+        print("Target not found")
+        
+        
+        
+        
+        
         
 t=BstTree()
 t.insert(10)
@@ -47,9 +80,11 @@ t.insert(1)
 t.insert(100)
 t.insert(0)
 t.inorder(t.root)
-        
-        
-        
+t.preorder(t.root)        
+t.postorder(t.root)     
+t.search(23,t.root)
+t.search(30,t.root)   
+t.search(0,t.root)
         
         
         
